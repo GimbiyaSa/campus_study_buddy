037f910a1937c6986f99601d64674597a4a106ac
Yabiso Wiseman Molefe <molefe.wiseman@gmail.com>
2025-09-22 22:26:15 +0200

    Allowing Swagger doc to access APIs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -29,6 +29,8 @@ app.use(
         process.env.FRONTEND_URL || '',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'http://127.0.0.1:8000',
+        'http://localhost:8000',
       ].filter(Boolean);
       // If no origin (same-origin or curl), allow it
       if (!origin) return callback(null, true);
